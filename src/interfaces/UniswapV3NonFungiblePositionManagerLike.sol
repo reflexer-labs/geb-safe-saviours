1ab6060095451642d73575e70cc273624845ae32
Guillaume Felley <guillaume@felley.io>
2021-11-24 12:23:10 +0100

    nit

--- src/interfaces/UniswapV3NonFungiblePositionManagerLike.sol
+++ src/interfaces/UniswapV3NonFungiblePositionManagerLike.sol
@@ -3,20 +3,6 @@ pragma solidity >=0.6.7;
 import "./IERC721.sol";
 
 abstract contract UniswapV3NonFungiblePositionManagerLike is IERC721 {
-    // struct CollectParams {
-    //     uint256 tokenId;
-    //     address recipient;
-    //     uint128 amount0Max;
-    //     uint128 amount1Max;
-    // }
-    // struct DecreaseLiquidityParams {
-    //     uint256 tokenId;
-    //     uint128 liquidity;
-    //     uint256 amount0Min;
-    //     uint256 amount1Min;
-    //     uint256 deadline;
-    // }
-
     function factory() public virtual view returns (address);
     function positions(uint256 tokenId)
         external
@@ -32,15 +18,4 @@ abstract contract UniswapV3NonFungiblePositionManagerLike is IERC721 {
           int24 tickUpper,
           uint128 liquidity
         );
-    // function collect(CollectParams calldata params)
-    //     external
-    //     payable
-    //     virtual
-    //     returns (uint256 amount0, uint256 amount1);
-
-    // function decreaseLiquidity(DecreaseLiquidityParams calldata params)
-    //     external
-    //     payable
-    //     virtual
-    //     returns (uint256 amount0, uint256 amount1);
 }
