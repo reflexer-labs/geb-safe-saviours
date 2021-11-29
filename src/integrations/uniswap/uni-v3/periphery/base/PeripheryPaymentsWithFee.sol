99777819f4a11f48a7990b6e7b049ecb48448103
Guillaume Felley <guillaume@felley.io>
2021-11-29 11:41:23 +0100

    Add swap router

---
+++ src/integrations/uniswap/uni-v3/periphery/base/PeripheryPaymentsWithFee.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.6.7;
+
+import '../../../../../interfaces/ERC20Like.sol';
+import '../../core/libraries/LowGasSafeMath.sol';
+
+import './PeripheryPayments.sol';
+import '../interfaces/IPeripheryPaymentsWithFee.sol';
+
+import '../../../../../interfaces/IWETH9.sol';
+import '../libraries/TransferHelper.sol';
+
+abstract contract PeripheryPaymentsWithFee is PeripheryPayments, IPeripheryPaymentsWithFee {
+    using LowGasSafeMath for uint256;
+
+    function unwrapWETH9WithFee(
+        uint256 amountMinimum,
+        address recipient,
+        uint256 feeBips,
+        address feeRecipient
+    ) public payable override {
+        require(feeBips > 0 && feeBips <= 100);
+
+        uint256 balanceWETH9 = IWETH9(WETH9).balanceOf(address(this));
+        require(balanceWETH9 >= amountMinimum, 'Insufficient WETH9');
+
+        if (balanceWETH9 > 0) {
+            IWETH9(WETH9).withdraw(balanceWETH9);
+            uint256 feeAmount = balanceWETH9.mul(feeBips) / 10_000;
+            if (feeAmount > 0) TransferHelper.safeTransferETH(feeRecipient, feeAmount);
+            TransferHelper.safeTransferETH(recipient, balanceWETH9 - feeAmount);
+        }
+    }
+
+    function sweepTokenWithFee(
+        address token,
+        uint256 amountMinimum,
+        address recipient,
+        uint256 feeBips,
+        address feeRecipient
+    ) public payable override {
+        require(feeBips > 0 && feeBips <= 100);
+
+        uint256 balanceToken = ERC20Like(token).balanceOf(address(this));
+        require(balanceToken >= amountMinimum, 'Insufficient token');
+
+        if (balanceToken > 0) {
+            uint256 feeAmount = balanceToken.mul(feeBips) / 10_000;
+            if (feeAmount > 0) TransferHelper.safeTransfer(token, feeRecipient, feeAmount);
+            TransferHelper.safeTransfer(token, recipient, balanceToken - feeAmount);
+        }
+    }
+}
